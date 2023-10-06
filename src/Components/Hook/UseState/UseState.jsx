83447256a80bd0b81e7d15efce1a0237effe6fbb
Rahul Varma <rahulvarma300@gmail.com>
2023-10-06 23:01:44 +0530

    Commented

--- src/Components/Hook/UseState/UseState.jsx
+++ src/Components/Hook/UseState/UseState.jsx
@@ -1,16 +1,18 @@
+// counting app using useState
 import { useState } from "react";
 
 function UseState(){
 
-    let [state, setState] = useState(0);
-    function click_Manage (){
-        setState(state + 1)
+    let [state, setState] = useState(0); // initiolising state
+
+    function click_Manage (){            // couting function
+        setState(state + 1)              // new value adding to state using setState
     }
 
     return(
         <>
         <h2 id="couter_heading" >Counter App</h2>
-            <button onClick={click_Manage} >Click Here</button>
+            <button onClick={click_Manage} >Click Here</button>   
             <h1> {state} </h1>
         </>
     )
