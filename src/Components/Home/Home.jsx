e9b81e5be53ebe1380d3959cf2f97c7925496d75
Rahul Varma <rahulvarma300@gmail.com>
2023-10-07 23:40:34 +0530

    Prps Passing

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,16 @@
 import UseState from '../Hook/UseState/UseState'
+import Props from '../Props/Props'
+
 function Home(){
+
+    let value = 'property value' // a variable created in Home page
     return(
+
         <>
             <h1>Home Page </h1>
             <UseState />
+            <h3 id='props_id'>Prps passing</h3>
+            <Props value={value} />
         </>
     )
 }
---
+++ src/Components/Props/Props.jsx
@@ -0,0 +1,7 @@
+function Props(prps){
+    return (
+       <p> <i>{prps.value}</i> passed from home to Props using property passing technic</p> // recevid prperty as argument from Home page
+    )
+}
+
+export default Props
